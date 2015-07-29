2560882068bd3faf549b23433978dc8b8f0383ec
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-07-29 15:37:40 +0200

    Issue-14 list module we want to compile with our docker compiler

--- compile.sh
+++ compile.sh
@@ -63,10 +63,63 @@ git submodule update
 
 # Compilation
 
+PROFILS_LIST=-Pcassandra,exclude-lucene,with-assembly,with-jetm 
+
+PROJECTS_LIST=org.apache.james:apache-james-mpt-imapmailbox-cassandra,\
+org.apache.james:apache-james-mpt-imapmailbox-cyrus,\
+org.apache.james:apache-james-mpt-external-james,\
+org.apache.james:apache-mime4j,\
+org.apache.james:apache-jsieve-all,\
+org.apache.james.jdkim:apache-jdkim,\
+org.apache.james.jspf:apache-jspf,\
+org.apache.james.jspf:apache-jspf-resolver,\
+org.apache.james.jspf:apache-jspf-tester,\
+org.apache.james.protocols:protocols-api,\
+org.apache.james.protocols:protocols-smtp,\
+org.apache.james.protocols:protocols-lmtp,\
+org.apache.james.protocols:protocols-netty,\
+org.apache.james.protocols:protocols-pop3,\
+org.apache.james.protocols:protocols-imap,\
+org.apache.james:apache-mailet-api,\
+org.apache.james:apache-mailet-base,\
+org.apache.james:mailetdocs-maven-plugin,\
+org.apache.james:apache-mailet-crypto,\
+org.apache.james:apache-mailet-standard,\
+org.apache.james:apache-mailet-ai,\
+org.apache.james:apache-james-mailbox-tika,\
+org.apache.james:apache-james-mailbox-elasticsearch,\
+org.apache.james:apache-james-mailbox-cassandra,\
+org.apache.james:james-server-util,\
+org.apache.james:james-server-cli,\
+org.apache.james:james-server-spring,\
+org.apache.james:james-server-core,\
+org.apache.james:james-server-lifecycle-api,\
+org.apache.james:james-server-mailbox-adapter,\
+org.apache.james:james-server-filesystem-api,\
+org.apache.james:james-server-mailetcontainer-api,\
+org.apache.james:james-server-mailetcontainer-camel,\
+org.apache.james:james-server-mailets,\
+org.apache.james:james-server-dnsservice-api,\
+org.apache.james:james-server-dnsservice-dnsjava,\
+org.apache.james:james-server-dnsservice-library,\
+org.apache.james:james-server-data-api,\
+org.apache.james:james-server-data-library,\
+org.apache.james:james-server-fetchmail,\
+org.apache.james:james-server-protocols-imap4,\
+org.apache.james:james-server-protocols-library,\
+org.apache.james:james-server-protocols-lmtp,\
+org.apache.james:james-server-protocols-pop3,\
+org.apache.james:james-server-protocols-smtp,\
+org.apache.james:james-server-queue-api,\
+org.apache.james:james-server-queue-file,\
+org.apache.james:james-server-queue-jms,\
+org.apache.james:james-server-queue-activemq
+
+
 if [ "$SKIPTESTS" = "skipTests" ]; then
-   mvn clean install -T1C -DskipTests -Pcassandra,exclude-lucene,with-assembly,with-jetm
+   mvn clean install -T1C -DskipTests $PROFILS_LIST -am -pl $PROJECTS_LIST
 else
-   mvn clean install -Pcassandra,exclude-lucene,with-assembly,with-jetm 
+    mvn clean install $PROFILS_LIST -am -pl $PROJECTS_LIST
 fi
 
 # Retrieve result
